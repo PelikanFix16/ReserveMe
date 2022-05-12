f0dfac69d090fe054c1e700e594c4edf4208bf13
PelikanFix16 <hubertosypowicz@outlook.com>
2022-05-12 21:12:58 +0200

    Entity class for marker
    This class have one functionality checking business rule

---
+++ Backend/SharedKernel/SharedKernel.Domain/EntityBase/Entity.cs
@@ -0,0 +1,13 @@
+using SharedKernel.Domain.BusinessRule;
+
+namespace SharedKernel.Domain.EntityBase
+{
+    public abstract class Entity
+    {
+        protected void CheckRule(IBusinessRule rule)
+        {
+            if (rule.IsBroken())
+                throw new BusinessRuleValidationException(rule);
+        }
+    }
+}
