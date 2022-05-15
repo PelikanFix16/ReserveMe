27c20980d23986c8f3e4ce363825c799f4e73b02
PelikanFix16 <hubertosypowicz@outlook.com>
2022-05-15 22:46:21 +0200

    User password value object for user context

---
+++ Backend/Components/User/User.Domain/ValueObjects/Password.cs
@@ -0,0 +1,25 @@
+using SharedKernel.Domain.BusinessRule.Rules;
+using SharedKernel.Domain.ValueObjects;
+
+namespace User.Domain.ValueObjects
+{
+    public class Password : ValueObject
+    {
+        public string Value { get; private set; }
+
+        private Password(string password)
+        {
+            Value = password;
+        }
+        public static Password Create(string password)
+        {
+            CheckRule(new PasswordMustBeStrongRule(password));
+            return new Password(password);
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
