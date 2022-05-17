4b61596e7c0ca392934d2ec8cc0acf014f34058c
PelikanFix16 <hubertosypowicz@outlook.com>
2022-05-17 18:42:21 +0200

    Rules for check status
    This rule check status was activated if yes throw exception, because user cannot confirm second time registration, but if is not confiormed this event confirm registration

---
+++ Backend/Components/User/User.Domain/UserRegistration/Rules/UserCannotBeConfirmedMoreThanOnceRule.cs
@@ -0,0 +1,17 @@
+using SharedKernel.Domain.BusinessRule;
+
+namespace User.Domain.UserRegistration.Rules
+{
+    public class UserCannotBeConfirmedMoreThanOnceRule : IBusinessRule
+    {
+        public string Message => "User Registration cannot be confirmed more than once";
+
+        private readonly UserStatus Status;
+
+        internal UserCannotBeConfirmedMoreThanOnceRule(UserStatus status){
+            Status = status;
+        }
+
+        public bool IsBroken() => Status == UserStatus.Activated;
+    }
+}
