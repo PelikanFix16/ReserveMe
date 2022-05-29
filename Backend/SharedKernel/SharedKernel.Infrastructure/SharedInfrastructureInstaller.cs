237bd2de92cec686da954f1925e348696b375f7e
PelikanFix16 <hubertosypowicz@outlook.com>
2022-05-29 18:36:18 +0200

    SharedKernel Infrastructure installer

---
+++ Backend/SharedKernel/SharedKernel.Infrastructure/SharedInfrastructureInstaller.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharedKernel.Application;
+using SharedKernel.Application.AggregateRepository;
+using SharedKernel.Application.EventBus;
+using SharedKernel.Infrastructure.EventRepository;
+using SharedKernel.Infrastructure.EventsBus;
+using SharedKernel.Infrastructure.EventStore;
+
+namespace SharedKernel.Infrastructure
+{
+    public static class SharedInfrastructureInstaller
+    {
+        public static IServiceCollection AddEventStoreMongoDb(this IServiceCollection service, IConfiguration configuration)
+        {
+
+            service.AddScoped<IMongoDbContext, MongoDbContext>();
+            service.AddScoped<IEventStore, MongoDbEventStore>();
+            return service;
+        }
+
+        public static IServiceCollection AddEventBus(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddOptions();
+            service.AddSingleton<IEventPublisher, EventPublisher>();
+            return service;
+        }
+
+        public static IServiceCollection AddEventRepository(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddOptions();
+            service.AddScoped<IEventRepository,EventRepositorySession>();
+            service.AddScoped<ISessionAggregate, SessionAggregate>();
+            return service;
+
+        }
+
+    }
+}
