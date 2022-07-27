8aa0ad89c5065aa2d5a76a35b6d2fe2a91d9efca
PelikanFix16 <hubertosypowicz@outlook.com>
2022-07-27 21:03:00 +0100

    Password rule refactor

--- Backend/SharedKernel/SharedKernel.Domain/BusinessRule/Rules/PasswordMustBeStrongRule.cs
+++ Backend/SharedKernel/SharedKernel.Domain/BusinessRule/Rules/PasswordMustBeStrongRule.cs
@@ -4,23 +4,24 @@ namespace SharedKernel.Domain.BusinessRule.Rules
 {
     public class PasswordMustBeStrongRule : IBusinessRule
     {
-        private readonly string Password;
+        private readonly string _password;
 
         public PasswordMustBeStrongRule(string password)
         {
-            Password = password;
+            _password = password;
         }
 
         public string Message => "Password is to weak";
 
         public bool IsBroken()
         {
-            if(string.IsNullOrEmpty(Password))
+            if (string.IsNullOrEmpty(_password))
                 return true;
+
             var hasNumber = new Regex(@"[0-9]+");
             var hasUpperChar = new Regex(@"[A-Z]+");
             var hasMinimum8Chars = new Regex(@".{8,}");
-            bool valid = hasNumber.IsMatch(Password) && hasUpperChar.IsMatch(Password) && hasMinimum8Chars.IsMatch(Password);
+            var valid = hasNumber.IsMatch(_password) && hasUpperChar.IsMatch(_password) && hasMinimum8Chars.IsMatch(_password);
             return !valid;
         }
     }
