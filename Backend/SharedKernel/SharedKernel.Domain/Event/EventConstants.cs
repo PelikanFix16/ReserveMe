58f4e5360ef3cde6bab2a31ad2b0489deeaf6798
PelikanFix16 <hubertosypowicz@outlook.com>
2022-07-28 17:40:09 +0100

    Event constants change to static class
    All fields in this class will be const, const is like static field so

--- Backend/SharedKernel/SharedKernel.Domain/Event/EventConstants.cs
+++ Backend/SharedKernel/SharedKernel.Domain/Event/EventConstants.cs
@@ -1,9 +1,7 @@
-#pragma warning disable RCS1102
 namespace SharedKernel.Domain.Event
 {
-    public class EventConstants
+    public static class EventConstants
     {
         public const string USER_REGISTERED = "UserRegisteredEvent";
     }
 }
-#pragma warning restore RCS1102
