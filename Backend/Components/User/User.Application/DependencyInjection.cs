e65b7486e9fdbc6856b9a85f99e13f63866ccadf
PelikanFix16 <hubertosypowicz@outlook.com>
2022-08-14 15:46:12 +0100

    User application dependency injection

---
+++ Backend/Components/User/User.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MediatR;
+
+namespace User.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddUserApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
