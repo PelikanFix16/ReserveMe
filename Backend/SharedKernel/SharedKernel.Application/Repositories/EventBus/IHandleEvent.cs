286695f7ecba5616c17727498f372024f04a0be5
PelikanFix16 <hubertosypowicz@outlook.com>
2022-10-01 20:29:52 +0100

    Not used for now HandleEvent

--- Backend/SharedKernel/SharedKernel.Application/Repositories/EventBus/IHandleEvent.cs
+++
@@ -1,9 +0,0 @@
-using SharedKernel.Domain.Event;
-
-namespace SharedKernel.Application.Repositories.EventBus
-{
-    public interface IHandleEvent
-    {
-        Task HandleAsync(DomainEvent @event);
-    }
-}
