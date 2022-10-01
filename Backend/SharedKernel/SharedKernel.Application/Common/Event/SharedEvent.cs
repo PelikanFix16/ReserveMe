dcdef82b0e7e33f576dcc5c3ddc7a42763bdbd01
PelikanFix16 <hubertosypowicz@outlook.com>
2022-10-01 20:29:34 +0100

    Shared Event for convert Domain Event
    Used event for conversion between domain event, when event is received we want receive all shared events, next parse this event to concrete domain event

---
+++ Backend/SharedKernel/SharedKernel.Application/Common/Event/SharedEvent.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharedKernel.Application.Common.Event
+{
+    public class SharedEvent
+    {
+        public string EventName { get; set; } = null!;
+        public string EventData { get; set; } = null!;
+    }
+}
