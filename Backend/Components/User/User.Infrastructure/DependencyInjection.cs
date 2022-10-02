236ab6d2a3a0c360bf5a140e301cd44d755457be
PelikanFix16 <hubertosypowicz@outlook.com>
2022-10-02 18:58:52 +0100

    Di for masstransit

--- Backend/Components/User/User.Infrastructure/DependencyInjection.cs
+++ Backend/Components/User/User.Infrastructure/DependencyInjection.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SharedKernel.Infrastructure;
+using User.Application.Consumers;
 
 namespace User.Infrastructure
 {
@@ -14,7 +16,18 @@ namespace User.Infrastructure
             this IServiceCollection services,
             ConfigurationManager configuration)
         {
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((context, rmqCfg) =>
+                {
+                    rmqCfg.ReceiveEndpoint(
+                        UserRegisteredEventConsumer.UserRegisteredEventName,
+                        e => e.Consumer<UserRegisteredEventConsumer>(context));
+                });
+            });
+            services.AddScoped<UserRegisteredEventConsumer>();
             services.AddSharedKernelInfrastructure(configuration);
+
             return services;
         }
     }
