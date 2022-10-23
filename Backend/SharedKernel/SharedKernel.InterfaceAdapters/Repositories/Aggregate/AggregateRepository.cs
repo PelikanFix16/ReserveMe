64d31c674ef3cd47779038a4fea725f42d35f6a4
PelikanFix16 <hubertosypowicz@outlook.com>
2022-10-23 18:50:32 +0100

    Remove not used usings

--- Backend/SharedKernel/SharedKernel.InterfaceAdapters/Repositories/Aggregate/AggregateRepository.cs
+++ Backend/SharedKernel/SharedKernel.InterfaceAdapters/Repositories/Aggregate/AggregateRepository.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using FluentResults;
 using SharedKernel.Application.Common.Errors.RepositoriesErrors;
-using SharedKernel.Application.Repositories.Aggregate;
+using SharedKernel.Application.Interfaces.Repositories;
 using SharedKernel.Domain.Aggregate;
 using SharedKernel.Domain.UniqueKey;
 using SharedKernel.InterfaceAdapters.Interfaces.Repositories;
