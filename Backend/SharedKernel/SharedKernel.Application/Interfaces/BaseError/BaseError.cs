939d11b0004dd791e2bad539efdce061a86e5035
PelikanFix16 <hubertosypowicz@outlook.com>
2022-10-25 22:00:09 +0100

    Base error inside Interfaces, its a abstract class
    We use interfaces folder to store abstract/interfaces objects so abstract is correct in this place

--- Backend/SharedKernel/SharedKernel.Application/Interfaces/BaseError/BaseError.cs
+++ Backend/SharedKernel/SharedKernel.Application/Interfaces/BaseError/BaseError.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using FluentResults;
 
-namespace SharedKernel.Application.Common.Errors
+namespace SharedKernel.Application.Interfaces.BaseError
 {
     public abstract class BaseError : Error
     {
