d4561f0fad58f8fe54c2b32fed7650c88a5feb9c
PelikanFix16 <hubertosypowicz@outlook.com>
2022-11-02 23:37:25 +0100

    UserRegisteredEvent consumer test save to database

--- Backend/Components/User/User.Application/Consumers/UserRegisteredEventConsumer.cs
+++ Backend/Components/User/User.Application/Consumers/UserRegisteredEventConsumer.cs
@@ -5,16 +5,33 @@ using System.Threading.Tasks;
 using MassTransit;
 using SharedKernel.Application.Common.Event;
 using SharedKernel.Domain.Event;
+using User.Application.Interfaces.Repositories;
+using User.Application.Projections;
 using User.Domain.User.Events;
 
 namespace User.Application.Consumers
 {
     public class UserRegisteredEventConsumer : IConsumer<UserRegisteredEvent>
     {
+        private readonly IUserProjectionRepository _repository;
+
+        public UserRegisteredEventConsumer(IUserProjectionRepository repository)
+        {
+            _repository = repository;
+        }
 
         public async Task Consume(ConsumeContext<UserRegisteredEvent> context)
         {
-            Console.WriteLine($"Event received: {context.Message.Name.FirstName}");
+            await Task.Delay(5000);
+            _repository.Save(new UserProjection
+            {
+                Id = context.Message.Key.Key,
+                Email = context.Message.Login.Value,
+                Password = context.Message.Password.Value,
+                Name = context.Message.Name.FirstName,
+                Surname = context.Message.Name.LastName
+            });
+            await _repository.SaveChangesAsync();
         }
     }
 }
