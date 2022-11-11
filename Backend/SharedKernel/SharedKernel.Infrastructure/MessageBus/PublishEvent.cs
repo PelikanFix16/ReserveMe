50d55fb7340a6ec2a55b529d997eaf274160fc2e
PelikanFix16 <hubertosypowicz@outlook.com>
2022-11-11 15:08:15 +0100

    Disable mass transit if DI not exists

--- Backend/SharedKernel/SharedKernel.Infrastructure/MessageBus/PublishEvent.cs
+++ Backend/SharedKernel/SharedKernel.Infrastructure/MessageBus/PublishEvent.cs
@@ -15,14 +15,23 @@ namespace SharedKernel.Infrastructure.MessageBus
         private readonly IPublishEndpoint _publishEndpoint;
         private readonly IEventConverter _converter;
 
-        public PublishEvent(IPublishEndpoint publishEndpoint, IEventConverter converter)
+        private readonly bool _notEnableMessageBus;
+
+
+        public PublishEvent(IEventConverter converter, IPublishEndpoint publishEndpoint = null)
         {
+            if (publishEndpoint is null)
+                _notEnableMessageBus = true;
+
             _publishEndpoint = publishEndpoint;
             _converter = converter;
         }
 
         public async Task PublishAsync(SharedEvent @event)
         {
+            if (_notEnableMessageBus)
+                return;
+
             var method = typeof(IEventConverter).GetMethod(nameof(IEventConverter.SharedEventToDomain));
             var generic = method.MakeGenericMethod(@event.EventTypeName);
             var domainEvent = generic.Invoke(_converter, new object[] { @event });
