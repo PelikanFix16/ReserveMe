3a473bbff20c5d8e8e45810cfcafb5f3bc92fcf0
PelikanFix16 <hubertosypowicz@outlook.com>
2022-11-13 16:59:37 +0100

    Check user exists in command handler cqrs

--- Backend/Components/User/User.Application/Commands/UserRegister/UserRegisterHandler.cs
+++ Backend/Components/User/User.Application/Commands/UserRegister/UserRegisterHandler.cs
@@ -3,7 +3,9 @@ using AutoMapper;
 using FluentResults;
 using MediatR;
 using SharedKernel.Application.Interfaces.Repositories;
+using User.Application.Interfaces.Repositories;
 using User.Application.Interfaces.Security;
+using User.Application.Mapper.Dto;
 using User.Domain.User;
 
 namespace User.Application.Commands.UserRegister
@@ -13,16 +15,27 @@ namespace User.Application.Commands.UserRegister
         private readonly IMapper _mapper;
         private readonly IAggregateRepository _aggregateRepository;
         private readonly ISecurityHash _passwordHash;
+        private readonly IUserProjectionRepository _repository;
 
-        public UserRegisterHandler(IMapper mapper, IAggregateRepository aggregateRepository, ISecurityHash passwordHash)
+        public UserRegisterHandler(
+            IMapper mapper,
+            IAggregateRepository aggregateRepository,
+            ISecurityHash passwordHash,
+            IUserProjectionRepository repository)
         {
             _mapper = mapper;
             _aggregateRepository = aggregateRepository;
             _passwordHash = passwordHash;
+            _repository = repository;
         }
 
         public async Task<Result<UserRegisterDto>> Handle(UserRegisterCommand request, CancellationToken cancellationToken)
         {
+            var login = _mapper.Map<LoginDto>(request.Login);
+            var userProjection = await _repository.GetAsync(login);
+            if (userProjection.IsSuccess)
+                return Result.Fail("User already exists");
+
             request.Password.Password = _passwordHash.HashPassword(request.Password.Password);
             var userAggregate = _mapper.Map<UserAggregateRoot>(request);
             _aggregateRepository.Save(userAggregate, userAggregate.Id);
