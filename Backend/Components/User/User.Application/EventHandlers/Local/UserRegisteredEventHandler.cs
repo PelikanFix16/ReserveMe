4d5d05ff711b5d6a22e0e66cbf275c84308fa646
PelikanFix16 <hubertosypowicz@outlook.com>
2022-11-13 16:59:53 +0100

    Save user when event is emitted localy

--- Backend/Components/User/User.Application/EventHandlers/Local/UserRegisteredEventHandler.cs
+++ Backend/Components/User/User.Application/EventHandlers/Local/UserRegisteredEventHandler.cs
@@ -2,17 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using SharedKernel.Application.Interfaces.Events;
+using User.Application.Interfaces.Services;
+using User.Application.Mapper.Projections;
 using User.Domain.User.Events;
 
 namespace User.Application.EventHandlers.Local
 {
     public class UserRegisteredEventHandler : IEventHandle<UserRegisteredEvent>
     {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserRegisteredEventHandler(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
         public async Task HandleAsync(UserRegisteredEvent @event)
         {
-            await Task.Delay(10000);
-            Console.WriteLine(@event.Name.FirstName);
+            var userProjection = _mapper.Map<UserProjection>(@event);
+            await _userService.UserCreateAsync(userProjection);
         }
     }
 }
