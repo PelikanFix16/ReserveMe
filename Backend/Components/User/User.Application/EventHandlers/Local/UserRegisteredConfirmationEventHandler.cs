66df00d90345d7cc570c422fa316a37cdae94ed1
PelikanFix16 <hubertosypowicz@outlook.com>
2022-11-20 01:17:06 +0100

    User registered confirmation event handler

---
+++ Backend/Components/User/User.Application/EventHandlers/Local/UserRegisteredConfirmationEventHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SharedKernel.Application.Interfaces.Events;
+using User.Application.Interfaces.Services;
+using User.Domain.User.Events;
+
+namespace User.Application.EventHandlers.Local
+{
+    public class UserRegisteredConfirmationEventHandler : IEventHandle<UserRegistrationConfirmedEvent>
+    {
+        private readonly IUserService _userService;
+
+        public UserRegisteredConfirmationEventHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task HandleAsync(UserRegistrationConfirmedEvent @event)
+        {
+            Console.WriteLine("Confirmed");
+            await _userService.UserConfirmAsync(@event.Key.Key);
+        }
+    }
+}
