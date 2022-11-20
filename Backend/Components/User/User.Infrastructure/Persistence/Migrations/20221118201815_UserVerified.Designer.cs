5f7a470b0b58f05cd0b3cd162cf32940509a2437
PelikanFix16 <hubertosypowicz@outlook.com>
2022-11-20 01:13:33 +0100

    new migrations for user projection

--- Backend/Components/User/User.Infrastructure/Persistence/Migrations/20221118201815_UserVerified.Designer.cs
+++ Backend/Components/User/User.Infrastructure/Persistence/Migrations/20221118201815_UserVerified.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using User.Infrastructure.Persistence.DataContext;
 
@@ -10,9 +11,10 @@ using User.Infrastructure.Persistence.DataContext;
 namespace User.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20221118201815_UserVerified")]
+    partial class UserVerified
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
